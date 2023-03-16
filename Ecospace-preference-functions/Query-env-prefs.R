67e003435f8c9abd7471a51b58bc255bd2bec7a8
Holden Earl Harris <holden.harris@noaa.gov>
2023-03-16 13:58:03 -0500

    Queried preferences from aquamaps. Aggregates data by species and functional groups.

--- Ecospace-preference-functions/Query-env-prefs.R
+++ Ecospace-preference-functions/Query-env-prefs.R
@@ -41,8 +41,73 @@ for (i in 1:nrow(fg)){
 fg <- fg %>% 
   filter(Key != '') %>% 
   distinct(Key, .keep_all = TRUE); nrow(fg)
+fg$ID_sp <- sprintf("Sp_%03d", 1:nrow(fg))
+
+## Write out species list
+write.csv(fg, "./global-data/speciesListGoM_QAQC_AMkeys.csv", row.names = F)
+
+##------------------------------------------------------------------------------
+## Query HSPEN environmental preferences
+## Some species have multiple entries. Without information to differentiate them
+## I've decided to average them.
+
+## Make long dataframe with unique key -----------------------------------------
+long_fg  <- data.frame()
+for (i in 1:nrow(fg)) {
+  #i = 4
+  row = fg[i, ]
+  key_ls  <- as.list(scan(text=fg$Key[i], what=""))
+  key_ls  <- key_ls[key_ls != "|"]
+  row$Key <- NULL
+  row     <- row %>% slice(rep(1:n(), each = length(key_ls)))
+  key_df  <- do.call(rbind.data.frame, key_ls)
+  names(key_df) <- "Key"
+  row     <- cbind(row, key_df)
+  long_fg <- rbind(long_fg, row)
+}
 
 
-  
-  
+## Make long dataframe with unique key -----------------------------------------
+## and query aquamap preferences
+long_pref <- data.frame()
+for (i in 1:nrow(long_fg)) {
+  #i = 4
+  key <- long_fg$Key[i]
+  prf <- am_hspen() %>% filter(SpeciesID == key) ## Query preferences from aquamaps
+  prf <- prf %>% 
+    select(LifeStage,   Pelagic,
+           DepthMin,    DepthPrefMin,    DepthPrefMax,    DepthMax,
+           TempMin,     TempPrefMin,     TempPrefMax,     TempMax,
+           SalinityMin, SalinityPrefMin, SalinityPrefMax, SalinityMax,
+           PrimProdMin, PrimProdPrefMin, PrimProdPrefMax, PrimProdPrefMax,
+           OxyMin,      OxyPrefMin,      OxyPrefMax,      OxyMax,
+           LandDistMin, LandDistPrefMin, LandDistPrefMax, LandDistMax,
+           NMostLat,    SMostLat,        WMostLong,       EMostLong)
+  long_pref <- rbind(long_pref, cbind(long_fg[i ,], prf))
+}
+
+## Aggregate by species
+summ_pref <- long_pref %>% 
+  group_by(ID_sp) %>% 
+  summarise(N_hspn = n(),
+            across(where(is.numeric), ~ round(mean(.x, na.rm = TRUE),2))
+  )
+              
+pref_bysp <- merge(fg, summ_pref, by = "ID_sp", all.x = T)
+pref_bysp <- pref_bysp[order(pref_bysp$ID_sp), ]
+
+## Aggregate by EwE FG
+pref_byfg <- pref_bysp %>% 
+  group_by(EwE_num, EwE_name) %>% 
+  summarise(
+    N_spp  = n(),
+    N_hspn = sum(N_hspn),
+    across(where(is.numeric), ~ round(mean(.x, na.rm = TRUE),2)),
+  )
+
+fg_pref         <- data.frame(EwE_num = unique(fg$EwE_num))
+fg_pref$rownum  <- 1:nrow(fg_pref)
+fg_pref         <- merge(fg_pref, pref_byfg, by = "EwE_num", all.x = T)
+fg_pref         <- fg_pref[order(fg_pref$rownum), ]
+fg_pref$rownum  <- NULL
 
