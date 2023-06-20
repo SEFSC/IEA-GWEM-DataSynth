9d8f808a842b828861a03f885430aeaae6bee595
Holden Earl Harris <holden.harris@noaa.gov>
2023-06-20 16:52:22 -0500

    Reran ChlA code

--- Ecospace-environmental-drivers/2-Make-depth-integrated-ChlA-maps-from-MODIS.R
+++ Ecospace-environmental-drivers/2-Make-depth-integrated-ChlA-maps-from-MODIS.R
@@ -5,13 +5,6 @@ library('marmap')
 library('stringr')
 library('viridis')
 
-dir.ras.in   <- "./Ecospace-environmental-drivers/MODIS/"
-dir.ras.out  <- "./Ecospace-environmental-drivers/Outputs/Bricks/"
-fld.asc.out  <- "./Ecospace-environmental-drivers/Outputs/ASCII-for-ecospace/"
-dir.asc.avg  <- "./Ecospace-environmental-drivers/Outputs/ASCII-for-ecospace/Averages/"
-dir.pdf.out  <- "./Ecospace-environmental-drivers/Outputs/PDF-maps/"
-depth08min   <- raster("./global-data/shorelinecorrected-basemap-depth-131x53-08 min-14sqkm.asc")
-
 
 ## -----------------------------------------------------------------------------
 ##
@@ -85,14 +78,26 @@ pdf_map = function(plt.stack, colscheme = "virid", dir, env_name, maxval){
 
 ##------------------------------------------------------------------------------
 ##
+## Set up directory paths
+
+dir.ras.in   <- "./Ecospace-environmental-drivers/MODIS/"
+dir.ras.out  <- "./Ecospace-environmental-drivers/Outputs/Bricks/"
+fld.asc.out  <- "./Ecospace-environmental-drivers/Outputs/ASCII-for-ecospace/"
+dir.asc.avg  <- "./Ecospace-environmental-drivers/Outputs/ASCII-for-ecospace/Averages/"
+dir.pdf.out  <- "./Ecospace-environmental-drivers/Outputs/PDF-maps/"
+depth08min   <- raster("./global-data/shorelinecorrected-basemap-depth-131x53-08 min-14sqkm.asc")
+
+
+
+##------------------------------------------------------------------------------
+##
 ## Make Ecospace env ASCII files for ChlZ - ChlA integrated euphotic depth
 
 env_driver = "ChlA"
 overwrite  = 'y'
 dir.asc.out = paste0(fld.asc.out, env_driver, "/")
 if(overwrite == 'y') {unlink(dir.asc.out, recursive = TRUE); dir.create(dir.asc.out)} 
-ras_hires = stack(paste0(dir.ras.in, env_driver, "/chlaxZe_-98_-80.5_24_31_200301-202207"))
-
+ras_hires = stack(paste0(dir.ras.in, tolower(env_driver), "/chlaxZe_-98_-80.5_24_31_200301-202207"))
 
 ## First, crop and resample to basemap grid 
 ras = crop(ras_hires, depth08min)
@@ -111,17 +116,15 @@ ras.dates$yrmo = paste0(ras.dates$year, "-", ras.dates$month)
 head(ras.dates); tail(ras.dates)
 asc.need = ras.dates$yrmo
 
-## Plot average 
-png(paste0("./maps/Mean_ChlZ.png"), width = 6, height = 8.5, units = "in", res = 1500)
-par(mfrow=c(2,1))
+## Average for intial map 
+mean <- stackApply(ras, indices =  rep(1,nlayers(ras)), fun = "mean", na.rm = T)
+
+par(mfrow=c(2,1)) ## Plot average 
 plot(mean, colNA = "black", main = "Averaged Chla Euphotic Depth")
 plot(log10(mean), colNA = "black", main = "Log10 Chla Euphotic Depth")
 par(mfrow=c(1,1))
-dev.off()
 
-
-################################################################################
-## Copy mean raster for months before data -------------------------------
+## Copy mean by month for months before data -----------------------------------
 mo = unique(ras.dates$month)
 enddummy = min(as.numeric(ras.dates$year))-1
 yr = 1980:enddummy
@@ -175,26 +178,135 @@ for (year in yr){
 
 ras.comb = stack(rep.stack, ras)
 
-#env_driver = "ChlA_Depth-integrated_MODIS"
 start = as.numeric(str_sub(names(ras.comb)[1], 2, 5))
 stop  = as.numeric(str_sub(names(ras.comb)[nlayers(ras.comb)], 2, 5))
-writeRaster(ras.comb, paste0(dir.ras.out, 'EwE_Maps_', env_driver, '_', start, '-', stop), overwrite=T)
 
 ## Write out files -------------------------------------------------------------
+## Save raster
+writeRaster(ras.comb, paste0(dir.ras.out, 'EwE_Maps_', env_driver, '_', start, '-', stop), overwrite=T)
 
-## ASCII
+## ASCII files by month
 writeRaster(ras.comb, paste0(dir.asc.out, env_driver), bylayer=T, suffix = names(ras.comb), 
             format = 'ascii', overwrite=T)
+
+## ASCII global average
+writeRaster(mean, paste0(dir.asc.avg, 'Avg_', env_driver),
+            bylayer=F, format='ascii', overwrite=T)
+
+## Make PDF of plots
+pdf_map(ras, dir = dir.pdf.out, env_name = env_driver, maxval = 200)
+pdf_map(log10(ras), dir = dir.pdf.out, env_name = "ChlA_log10", maxval = log10(390))
+
+
+################################################################################
+##
+## Cfl
+
+env_driver = "Cfl"
+overwrite  = 'y'
+dir.asc.out = paste0(fld.asc.out, env_driver, "/")
+if(overwrite == 'y') {unlink(dir.asc.out, recursive = TRUE); dir.create(dir.asc.out)} 
+ras_hires = stack(paste0(dir.ras.in, tolower(env_driver), "/chlaxZe_-98_-80.5_24_31_200301-202207"))
+
+## First, crop and resample to basemap grid 
+ras = crop(ras_hires, depth08min)
+ras = resample(ras, depth08min)
+dim(depth08min); dim(ras) ## Dimensions should match
+
+## Plotcheck
+par(mfrow=c(2,1))
+plot(depth08min, colNA = 'black')
+plot(ras[[1]], colNA = 'black')
+par(mfrow=c(1,1))
+
+## Determine which ones need to be written out
+ras.dates = data.frame(year=substr(names(ras),2,5),month=substr(names(ras),6,7))
+ras.dates$yrmo = paste0(ras.dates$year, "-", ras.dates$month)
+head(ras.dates); tail(ras.dates)
+asc.need = ras.dates$yrmo
+
+## Average for intial map 
+mean <- stackApply(ras, indices =  rep(1,nlayers(ras)), fun = "mean", na.rm = T)
+
+par(mfrow=c(2,1)) ## Plot average 
+plot(mean, colNA = "black", main = "Averaged Chla Euphotic Depth")
+plot(log10(mean), colNA = "black", main = "Log10 Chla Euphotic Depth")
+par(mfrow=c(1,1))
+
+## Copy mean by month for months before data -----------------------------------
+mo = unique(ras.dates$month)
+enddummy = min(as.numeric(ras.dates$year))-1
+yr = 1980:enddummy
+
+## Make dataframe of year months before MODIS data starts
+dummy.dates = data.frame(year = character(), month = character())
+for(y in yr){
+  for(m in mo){
+    dummy.dates = rbind(dummy.dates, c(y,m))
+  }
+}
+colnames(dummy.dates) = c("year", "month")
+dummy.dates$yrmo = paste(dummy.dates$year, dummy.dates$month, sep="-")
+head(dummy.dates); tail(dummy.dates)
+
+## Replicate average for months Jan 1980 until MODIS data starts in 2003
+dummy.ras = brick(replicate(nrow(dummy.dates), mean))
+names(dummy.ras) = dummy.dates$ym
+
+## Rename layers: e.g., "X200301" to "X2003.01" to extract months
+layers = names(ras_hires)
+names(ras) = sub("(.{5})(.*)", "\\1.\\2", layers) ## Capture the first 5 characters as a group ((.{5})) followed by one or more characters in another capture group ((.*)) and then replace with the backreference of first group (\\1) followed by a . followed by second backreference (\\2).
+names(ras)
+
+## Get monthly averages --------------------------------------------------------
+month.stack = stack()
+for (month in mo){
+  #month = "01"
+  subset.month = raster::subset(ras, grep(paste0('.', month), names(ras), value = T, fixed = T))
+  names(subset.month)
+  month.avg = stackApply(subset.month, indices =  rep(1,nlayers(subset.month)), fun = "mean", na.rm = T)
+  names(month.avg) = paste0("Mo", month, "_avg", nlayers(subset.month),"y")
+  month.stack = addLayer(month.stack, month.avg)
+}  
+
+## Plot check
+par(mfrow=c(3,4))
+plot(month.stack, colNA = 'black', 
+     zlim=c(min(values(month.stack), na.rm=T), max(values(month.stack), na.rm=T))
+)
+par(mfrow=c(1,1))
+
+## Combine dummy raster set (1980-2002) and data (2003-2022) -------------------
+rep.stack = stack()
+for (year in yr){
+  #year = 1980
+  xx = month.stack
+  names(xx) = paste0(year, "_", substr(labels(month.stack), 3, 8))
+  rep.stack = addLayer(rep.stack, xx)
+}
+
+ras.comb = stack(rep.stack, ras)
+
+start = as.numeric(str_sub(names(ras.comb)[1], 2, 5))
+stop  = as.numeric(str_sub(names(ras.comb)[nlayers(ras.comb)], 2, 5))
+
+## Write out files -------------------------------------------------------------
+## Save raster
+writeRaster(ras.comb, paste0(dir.ras.out, 'EwE_Maps_', env_driver, '_', start, '-', stop), overwrite=T)
+
+## ASCII files by month
+writeRaster(ras.comb, paste0(dir.asc.out, env_driver), bylayer=T, suffix = names(ras.comb), 
+            format = 'ascii', overwrite=T)
+
+## ASCII global average
+writeRaster(mean, paste0(dir.asc.avg, 'avg_chla_integrated_euphotic_depth'),
+            bylayer=F, format='ascii', overwrite=T)
 
 ## Make PDF of plots
 pdf_map(ras, dir = dir.pdf.out, env_name = "ChlA", maxval = 200)
 pdf_map(log10(ras), dir = dir.pdf.out, env_name = "ChlA_log10", maxval = log10(390))
 
 
-## Average for intial map 
-mean <- stackApply(ras, indices =  rep(1,nlayers(ras)), fun = "mean", na.rm = T)
-writeRaster(mean, paste0(dir.asc.avg, 'avg_chla_integrated_euphotic_depth'),
-            bylayer=F, format='ascii', overwrite=T)
 
 
 
@@ -217,9 +329,13 @@ writeRaster(mean, paste0(dir.asc.avg, 'avg_chla_integrated_euphotic_depth'),
 
 
 
-################################################################################
-##
-## Cfl
+
+
+
+
+
+
+
 
 env_driver = "cfl"
 overwrite  = 'y'
