bd94d72882a2efa5ef464a14001feff1bcc32532
holdenharris-NOAA <holden.harris@noaa.gov>
2023-07-10 17:05:13 -0500

    - Updated code for making pref function plots. These now use a doubl loop to make pages within a PDF. Code makes plots for all three env. variables or each one alone. - PDF plots uploaded to GitHub

--- Ecospace-preference-functions/3-Make-env-matrices-for-Ecospace.R
+++ Ecospace-preference-functions/3-Make-env-matrices-for-Ecospace.R
@@ -2,9 +2,11 @@
 ##
 ## Make matrices for Ecospace
 
+## Setup -----------------------------------------------------------------------
+rm(list=ls()); gc()
+source("./Ecospace-preference-functions/2-Doublelogistic-env-pref-functions.R")
 fg_pref <- read.csv("./Ecospace-preference-functions/intermediate-ouput/fg-env-preference-parameters-adjusted.csv")
 dir_out <- "./Ecospace-preference-functions/output-for-Ecospace/"
-source("./Ecospace-preference-functions/2-Doublelogistic-env-pref-functions.R")
 
 ## Depth -----------------------------------------------------------------------
 driver = "Depth"
@@ -22,7 +24,6 @@ colnames(depth_mat) = fg_pref$EwE_name
 
 ## Loop through functional groupos
 for (i in 1:ncol(depth_mat)){
-  
   ## Set the four HSPEN parameters
   absmin = fg_pref$DepthMin[i]
   prfmin = fg_pref$DepthPrefMin[i]
@@ -54,7 +55,7 @@ pref_mat = matrix(nrow = 1203, ncol = nrow(fg_pref))
 row.names(pref_mat) = c("Name", "Left_limit", "Right_limit", 1:1200)
 colnames(pref_mat) = fg_pref$EwE_name
 
-## Loop through functional groupos
+## Loop through functional groups
 for (i in 1:ncol(pref_mat)){
   #  i = 2
   absmin = fg_pref$TempMin[i]
@@ -110,90 +111,116 @@ write.csv(pref_mat, paste0(dir_out, driver, "-pref-func-", ncol(depth_mat), "fg-
 
 ## -----------------------------------------------------------------------------
 ## Make plots
-nwide = 6
-nhigh = 6
+
+dir_pdf = "./Ecospace-preference-functions/figures/"
+n_rows = 6 ## Number of plots per row on a page
+n_cols = 6 ## Number of plots per column on a page
 pg_plts = nwide * nhigh
 n_plots = nrow(fg_pref) * 3
 max_depth = 400
 max_temp = 40
 max_sal = 40
-
 w = 8.5
 h = 11 
-pdf("./Ecospace-preference-functions/figures/Pref-functions-depth-temp-sal.pdf", width = w, height = h, 
-    onefile = T)
-for(i in seq(1, n_plots, pg_plts)) {
-  par(mfrow=c(6,6))
-  for(j in seq(i, i+35)) {
+
+## Scale X-axis
+pdf(paste0(dir_pdf, "Pref-functions_depth-temp-sal_byFG_xaxis-scaled.pdf"), 
+           width = w, height = h, onefile = T)
+for(i in seq(from = 1, to = n_plots, by = pg_plts)) {
+#  i = 1
+  par(mfrow=c(n_rows, n_cols))
+  for(j in seq(from = i, to = i+(n_rows * n_cols - 1))) {
+    ## Plot depth
     plot_pref_func(fg_pref$DepthMin[j], fg_pref$DepthPrefMin[j], 
                    fg_pref$DepthPrefMax[j], fg_pref$DepthMax[j], 
                    fg_num = fg_pref$EwE_num[j], 
                    fg_name = fg_pref$EwE_name[j],
-                   max = max_depth, scale_xaxis = 'y', range = 'wide',
+                   max = max_depth, scale_xaxis = 'y', 
+                   range = 'wide',
                    driver = "Depth")
-    
+    ## Plot temperature
     plot_pref_func(fg_pref$TempMin[j], fg_pref$TempPrefMin[j], 
                    fg_pref$TempPrefMax[j], fg_pref$TempMax[j], 
                    fg_num = fg_pref$EwE_num[j], 
                    fg_name = fg_pref$EwE_name[j],
-                   max = max_temp, scale_xaxis = 'y', range = 'nar',
+                   max = max_temp, scale_xaxis = 'y', 
+                   range = 'nar',
                    driver = "Temp")
-    
+    ## Plot salinity
     plot_pref_func(fg_pref$SalinityMin[j], fg_pref$SalinityPrefMin[j], 
                    fg_pref$SalinityPrefMax[j], fg_pref$SalinityMax[j], 
                    fg_num = fg_pref$EwE_num[j], 
                    fg_name = fg_pref$EwE_name[j],
-                   max = max_sal, scale_xaxis = 'y', range = 'nar',
+                   max = max_sal, scale_xaxis = 'y', 
+                   range = 'nar',
                    driver = "Sal")
+    
+  }
+}
+dev.off()
+
+## Set X-axis
+pdf(paste0(dir_pdf, "Pref-functions_depth-temp-sal_byFG_xaxis-set.pdf"), 
+    width = w, height = h, onefile = T)
+for(i in seq(from = 1, to = n_plots, by = pg_plts)) {
+  par(mfrow=c(n_rows, n_cols))
+  for(j in seq(from = i, to = i+(n_rows * n_cols - 1))) {
+    ## Plot depth
+    plot_pref_func(fg_pref$DepthMin[j], fg_pref$DepthPrefMin[j], 
+                   fg_pref$DepthPrefMax[j], fg_pref$DepthMax[j], 
+                   fg_num = fg_pref$EwE_num[j], 
+                   fg_name = fg_pref$EwE_name[j],
+                   max = max_depth, scale_xaxis = 'n', 
+                   range = 'wide',
+                   driver = "Depth")
+    ## Plot temperature
+    plot_pref_func(fg_pref$TempMin[j], fg_pref$TempPrefMin[j], 
+                   fg_pref$TempPrefMax[j], fg_pref$TempMax[j], 
+                   fg_num = fg_pref$EwE_num[j], 
+                   fg_name = fg_pref$EwE_name[j],
+                   max = max_temp, scale_xaxis = 'n', 
+                   range = 'nar',
+                   driver = "Temp")
+    ## Plot salinity
+    plot_pref_func(fg_pref$SalinityMin[j], fg_pref$SalinityPrefMin[j], 
+                   fg_pref$SalinityPrefMax[j], fg_pref$SalinityMax[j], 
+                   fg_num = fg_pref$EwE_num[j], 
+                   fg_name = fg_pref$EwE_name[j],
+                   max = max_sal, scale_xaxis = 'n', 
+                   range = 'nar',
+                   driver = "Sal")
+    
+  }
+}
+dev.off()
+
+## Plot depth with varying X-axis (depth) --------------------------------------
+n_plots = nrow(fg_pref) 
+pdf(paste0(dir_pdf, "Pref_depth_xaxis-scaled.pdf"), width = w, height = h, onefile = T)
+for(i in seq(from = 1, to = n_plots, by = pg_plts)) {
+  for(j in seq(from = i, to = i+(n_rows * n_cols - 1))) {
+    ## Plot depth
+    plot_pref_func(fg_pref$DepthMin[j], fg_pref$DepthPrefMin[j], 
+                   fg_pref$DepthPrefMax[j], fg_pref$DepthMax[j], 
+                   fg_num = fg_pref$EwE_num[j], 
+                   fg_name = fg_pref$EwE_name[j],
+                   max = max_depth, scale_xaxis = 'n', 
+                   range = 'wide',
+                   driver = "Depth")
   }
 }
 dev.off()
 
 
-par(mfrow=c(6,6))
-for(j in 1:12){
-  plot_pref_func(fg_pref$DepthMin[j], fg_pref$DepthPrefMin[j], 
-                 fg_pref$DepthPrefMax[j], fg_pref$DepthMax[j], 
-                 fg_num = fg_pref$EwE_num[j], 
-                 fg_name = fg_pref$EwE_name[j],
-                 max = max_depth, scale_xaxis = 'y', range = 'wide',
-                 driver = "Depth")
-  
-  plot_pref_func(fg_pref$TempMin[j], fg_pref$TempPrefMin[j], 
-                 fg_pref$TempPrefMax[j], fg_pref$TempMax[j], 
-                 fg_num = fg_pref$EwE_num[j], 
-                 fg_name = fg_pref$EwE_name[j],
-                 max = max_temp, scale_xaxis = 'y', range = 'nar',
-                 driver = "Temp")
-  
-  plot_pref_func(fg_pref$SalinityMin[j], fg_pref$SalinityPrefMin[j], 
-                 fg_pref$SalinityPrefMax[j], fg_pref$SalinityMax[j], 
-                 fg_num = fg_pref$EwE_num[j], 
-                 fg_name = fg_pref$EwE_name[j],
-                 max = max_sal, scale_xaxis = 'y', range = 'nar',
-                 driver = "Sal")
-}
 
 
 
-
-
-## Plot depth with varying X-axis (depth) --------------------------------------
-max = 400
-w = 8.5
-h = 11 
-pdf("./Ecospace-preference-functions/figures/Depth-pref-dbl-logistic_xaxis-set.pdf", width = w, height = h, 
-    onefile = T)
-par(mfrow=c(6,6))
-
-max <- 400
 for(j in 1:36){
-  i = 1
   p1 <- fg_pref$DepthMin[j] 
   p2 <- fg_pref$DepthPrefMin[j] 
   p3 <- fg_pref$DepthPrefMax[j] 
   p4 <- fg_pref$DepthMax[j] 
-  plot_preference_function(p1, p2, p3, p4, 
+  plot_pref_function(p1, p2, p3, p4, 
                            fg_num = fg_pref$EwE_num[j], 
                            fg_name = fg_pref$EwE_name[j],
                            max = max, scale_xaxis = 'n', range = 'wide')
@@ -204,7 +231,7 @@ for(j in 37:72){
   p2 <- fg_pref$DepthPrefMin[j] 
   p3 <- fg_pref$DepthPrefMax[j] 
   p4 <- fg_pref$DepthMax[j] 
-  plot_preference_function(p1, p2, p3, p4, 
+  plot_pref_function(p1, p2, p3, p4, 
                            fg_num = fg_pref$EwE_num[j], 
                            fg_name = fg_pref$EwE_name[j],
                            max = max, 
@@ -225,7 +252,7 @@ for(j in 1:36){
   p2 <- fg_pref$TempPrefMin[j] 
   p3 <- fg_pref$TempPrefMax[j] 
   p4 <- fg_pref$TempMax[j] 
-  plot_preference_function(p1, p2, p3, p4, 
+  plot_pref_function(p1, p2, p3, p4, 
                            fg_num = fg_pref$EwE_num[j], 
                            fg_name = fg_pref$EwE_name[j],
                            max = max, 
@@ -237,7 +264,7 @@ for(j in 37:72){
   p2 <- fg_pref$TempPrefMin[j] 
   p3 <- fg_pref$TempPrefMax[j] 
   p4 <- fg_pref$TempMax[j] 
-  plot_preference_function(p1, p2, p3, p4, 
+  plot_pref_function(p1, p2, p3, p4, 
                            fg_num = fg_pref$EwE_num[j], 
                            fg_name = fg_pref$EwE_name[j],
                            max = max, 
@@ -258,7 +285,7 @@ for(j in 1:36){
   p2 <- fg_pref$SalinityPrefMin[j] 
   p3 <- fg_pref$SalinityPrefMax[j] 
   p4 <- fg_pref$SalinityMax[j] 
-  plot_preference_function(p1, p2, p3, p4, 
+  plot_pref_function(p1, p2, p3, p4, 
                            fg_num = fg_pref$EwE_num[j], 
                            fg_name = fg_pref$EwE_name[j],
                            max = max, xmin = 15,
@@ -270,7 +297,7 @@ for(j in 37:72){
   p2 <- fg_pref$SalinityPrefMin[j] 
   p3 <- fg_pref$SalinityPrefMax[j] 
   p4 <- fg_pref$SalinityMax[j] 
-  plot_preference_function(p1, p2, p3, p4, 
+  plot_pref_function(p1, p2, p3, p4, 
                            fg_num = fg_pref$EwE_num[j], 
                            fg_name = fg_pref$EwE_name[j],
                            max = max, xmin = 15,
