b52aa00ed82001f3034b37d203df850be5f582bb
holdenharris-NOAA <holden.harris@noaa.gov>
2024-04-02 13:43:08 -0400

    Test push to SEFSC

--- Ecospace-environmental-drivers/B1-get-HYCOM.R
+++ Ecospace-environmental-drivers/B1-get-HYCOM.R
@@ -7,7 +7,7 @@ bbox.wfs = c(-88, -80.5, 25, 30.5)
 bbox = bbox.gom
 dir.hycom = "D:/HYCOM satelite imagery/"
 ##--> Sets download to external hard drive due to space requirements
-
+##
 ## -----------------------------------------------------------------------------
 ##
 ## 1) Get list of hycom files on ftp server
